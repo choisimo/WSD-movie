36860eefe4f15679bd4c6bda9e1f361f780625f4
nodove <150008602+choisimo@users.noreply.github.com>
2024-11-12 15:17:10 +0900

    add loading

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,12 @@
 import AppRoutes from "AppRoutes";
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router, useLocation} from 'react-router-dom';
 import Header from 'content/layout/header';
 import Footer from 'content/layout/footer';
 import './App.css';
 
-
 function App() {
-  return (
+
+    return (
     <div className="App">
       <Router>
           <Header />
--- react-app/src/AppRoutes.js
+++ react-app/src/AppRoutes.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {Routes, Route, useLocation} from 'react-router-dom';
 import routes from './routes.json';
 import SignIn from 'content/views/pages/SignIn/SignIn';
 import Dashboard from 'content/views/pages/Dashboard';
@@ -7,9 +7,28 @@ import HomePage from 'content/views/pages/home/home';
 import ProtectionRouter from 'content/components/security/protectionRouter';
 import MovieDetailPage from 'content/views/pages/detailView/MovieDetailPage';
 import NotFound from 'content/views/pages/error/404'; // 404 페이지 컴포넌트
+import LoadingSpinner from "content/components/utility/LoadingSpinner";
 
 const AppRoutes = () => {
+
+    const [loading, setLoading] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        const handleStartLoading = () => setLoading(true);
+        const handleStopLoading = () => setLoading(false);
+
+        // 페이지 변경 감지
+        handleStartLoading();
+        const timeoutId = setTimeout(handleStopLoading, 200); // 로딩 0.5초 후 종료
+
+        return () => clearTimeout(timeoutId);
+    }, [location]);
+
+
     return (
+        <div>
+            {loading && <LoadingSpinner />}
         <Routes>
             {/* 공개된 경로 */}
             <Route path={routes.SignIn} element={<SignIn />} />
@@ -36,6 +55,7 @@ const AppRoutes = () => {
             {/* 잘못된 경로는 404 페이지로 리다이렉트 */}
             <Route path="*" element={<NotFound />} />
         </Routes>
+        </div>
     );
 };
 
